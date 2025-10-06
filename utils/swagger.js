da2b104fc0da1d365c3ab66bb268ff1f55d29ab2
aelaraby6 <body1262005@gmail.com>
2025-10-06 18:25:54 +0300

    first commit

---
+++ utils/swagger.js
@@ -0,0 +1,35 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "",
+      version: "1.0.0",
+      description: "",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ["./docs/**/*.js"], 
+
+}
+
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export { swaggerUi, swaggerSpec };
